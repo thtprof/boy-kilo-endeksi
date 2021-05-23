cdcd2d72f07c5ed7ccf1e1fef95f3c97e36e0948
PROFESÖR THT <77558299+thtprof@users.noreply.github.com>
2021-05-23 16:30:03 +0300

    Update boy-kilo-endeksi.py

--- boy-kilo-endeksi.py
+++ boy-kilo-endeksi.py
@@ -17,7 +17,7 @@ def hesapla():
     boy = float(input("Boyunuzu giriniz:"))
     kilo = float(input("Kilonuzu giriniz:"))
     endeks = kilo/((boy/100)**2)
-    baskı ("Endeks Ortalamaniz:", endeks )
+    print(f"Endeks Ortalamaniz: {endeks}")
 
     if endeks <= 18.4:
         print ("Endeksiniz: Zayif")
